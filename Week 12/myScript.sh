b63570f445d90f1f0fab71e4c2b97dafe60da11f
inioluwa7 <inioluwaolarinoye4@gmail.com>
2024-07-24 21:12:25 -0400

    Adding Bash Scripts

---
+++ Week 12/myScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A simple script that uses variables - Inioluwa
+echo "I am logged into the $USER account"
+echo "scripts file name is $0"
+echo "Linux is $1"
+exit
---
+++ Week 12/myScript2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# A script that uses conditional expressions and loops - Inioluwa
+count=10
+while [ $count -gt 0 ]
+do
+	echo the loop count is $count
+	count=$[ $count - 1 ]
+done
+#
+if [ $count -eq 0 ]
+then
+	echo "Count is set to zero: $count"
+else
+	echo "Count is not set to zero: $count"
+fi
+exit
